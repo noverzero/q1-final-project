fbd0fd354ee152d70eb60026079f3cd4b1dbee6f
dustinhuth <dustin@undefinedindustries.com>
2018-11-07 07:25:59 -0700

    lots of prettying up/responsiveness.  used local storage to store eventArr for faster load on refresh after first visit.  added table sort to sort by date.  started on fuel savings calculations.

---
+++ localstorage.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+  console.log("developed by undefinedindustries.com")
+  localStorage.setItem("array" , JSON.stringify([1,2,5,4]))
+  let myData = JSON.parse(localStorage.getItem("array"))
+  console.log(myData)
+
+//fiurst get the item from local localStorage
+//then push onto that arrauy
+//setitem back into local storage
+
+
+let list = []
+
+
+//get the values from the form
+//make an object that contains all of those values
+//store them in localstorage using setItem... make sure to stringify
+
+//store the value that signifies the colunn
+//write a function that takes sort parameter and outputs the correctly sorted table
+
+
+//--------------------------
+//pass the sort function a column as a parameter::
+function sortTable(column){
+
+}
+})
--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(){ console.log("developed by undefinedindustries.com")
 
+
+
+//create 'calculate' button event listener and calculate function.
+let calculate = document.getElementById('calculate')
+calculate.addEventListener("click" , function(){
+  console.log("I hear a click!")
+
+
 //Server call for event data
     axios.get('https://www.eventbriteapi.com/v3/users/me/owned_events/?token=T4AWNLBLNDQD32SYGHHI&order_by=start_desc&expand=ticket_classes')
     .then(function(response){
@@ -8,6 +16,9 @@ document.addEventListener("DOMContentLoaded", function(){ console.log("developed
 //create a variable and set it equal to our first level of relevant data for easy access
       const eventsArr = response.data.events
       console.log("eventsArr!!!!" , eventsArr)
+
+      localStorage.setItem("array" , JSON.stringify(eventsArr))
+
 
 //create an array of headliner names
       let headlinersArr = []
@@ -62,6 +73,78 @@ document.addEventListener("DOMContentLoaded", function(){ console.log("developed
       }
       getRidersTotal()
 
+      //write a function that takes the eventsArr and returns an array of objects with key = pick-up location name, value = quantity_sold
+
+
+      // let ridersByLocationArr = []
+      // let ridersByLocationObj = {}
+      // function getRidersByLocation(){
+      //
+      //   for(let ii = 0; ii < eventsArr.length; ii++){
+      //     let classes = eventsArr[ii].ticket_classes
+      //     console.log("classes $$$$$$" , classes)
+      //     for (let ii = 0; ii < classes.length; ii++){
+      //      ridersByLocationObj[classes[ii].name.substring(indexof(), 15)] = classes[ii].quantity_sold
+      //     console.log("ridersByLocationObj |||||||||||" , ridersByLocationObj)
+      //     //console.log("ridersByLocation*****" , ridersByLocationObj)
+      //     //ridersByLocationArr.push(ridersByLocationObj)
+      //     }
+      //   }
+      // }
+      // getRidersByLocation()
+
+
+
+
+
+
+      //calculate fuel savings
+      //for each event get total number of riders from each location
+      //create object of roundtrip miles for each location
+      let miles = {
+        boulderRrx: 54.2,
+        focoRrx: 145.4,
+        denverRrx: 43.2,
+        boulderDen: 54.8,
+        focoDen: 126.4,
+        denverDen: 0,
+        boulder1stbank: 25.8,
+        foco1stbank: 99.6,
+        denver1stbank: 31.6,
+      }
+
+      //create object of MPG and average capacity for bus
+      let bus = {
+        mpg: 7,
+        cap: 48,
+      }
+      //create object of MPG and average capacity for car (this will be editable by user)
+      let car={
+        mpg: 30,
+        cap: 3,
+      }
+      //calculate bus fuel consumed:
+      //number of buses used = number of riders / average bus capacity
+        let busesUsed = Math.ceil(totalRidersArr[1] / bus.cap)
+      console.log("busesUsed --->>" , busesUsed)
+      //fuel per bus = roundtrip miles / bus mpg
+
+      //bus fuel consumed per event = fuel per bus * number of buses used
+      //
+      //calculate parallel universe car fuel consumed
+      //number of cars used = number of riders / average car capacity
+      //fuel per car = roundtrip miles / car MPG
+      //car fuel consumed per event = fuel per car * number of cars used
+      //
+      //calculate fuel savings for each event
+      //fuel not consumed = parallel universe fuel consumed per event - bus fuel consumed per event
+      //
+      //total fuel saved = sum of fuel not consumed throughout all events in question.
+
+      //only display date range (Last 50, 2018, Live, top 10, userInput)
+      //only display headliner === userInput
+
+
 //Build the table!
 
       function createMainRows(){
@@ -102,7 +185,7 @@ document.addEventListener("DOMContentLoaded", function(){ console.log("developed
       createMainRows()
 
       function sortTable() {
-        var table, rows, switching, i, x, y, shouldSwitch;
+        let table, rows, switching, i, x, y, shouldSwitch;
         table = document.getElementById("myTable");
         switching = true;
         /*Make a loop that will continue until
@@ -137,28 +220,259 @@ document.addEventListener("DOMContentLoaded", function(){ console.log("developed
       }
       sortTable()
 
-      //create object with key = pick-up location name, value = quantity_sold
-      // ridersByLocation[ticketClasses[ii].name.substring(5, 10)] = ticketClasses[ii].quantity_sold
-      // let ridersByLocation = {}
-      // console.log("ridersByLocation*****" , ridersByLocation)
+
+
+
+
+
 
     })
-  //function findHeadliner()
 
-  // axios.get('https://www.eventbriteapi.com/v3/users/me/owned_events/?token=T4AWNLBLNDQD32SYGHHI&status=live&order_by=start_desc&expand=ticket_classes')
-  // .then(function(response){
-  //   console.log("this is response.data::" , response.data)
-  //   for(let ii=0; ii < response.data.events.length; ii++){
-  //     let myEvent = response.data.events[ii]
-  //           console.log("this is myEvent.name.text::" , myEvent.name.text)
-  //           let headliner = myEvent.name.text.substring((0), myEvent.name.text.indexOf("*")-1)
-  //           console.log("this is headliner:::" , headliner)
-  //           // var res = str.substring(str.indexOf("e"), str.indexOf("o"))
-  //           for(let ii=0; ii < myEvent.ticket_classes.length; ii++){
-  //                 let ticketClass = myEvent.ticket_classes[ii]
-  //                 console.log("this is ticketClass.name::" , ticketClass.name, "this is quantity sold::" , ticketClass.quantity_sold)
-  //               }
-  //   }
-  // })
+
+
+
+
+  })
+
+
+
+
+
+
+
+
+  function loadStored(){
+  //retreive previous stored eventsArr from local storage
+    let storedEvents = JSON.parse(localStorage.getItem("array"))
+    console.log("storedEvents:o :o :o :o" , storedEvents)
+
+    if(storedEvents){
+      alert("yep!  storedEvents would work!")
+      //create an array of headliner names
+            let headlinersArr = []
+            function getHeadliners(){
+              for(let ii = 0; ii < storedEvents.length; ii++){
+              headlinersArr.push(storedEvents[ii].name.text.substring((0), storedEvents[ii].name.text.indexOf("*")-1))
+              }
+              return headlinersArr
+            }
+            //getHeadliners()
+            console.log("getHeadliners():::" , getHeadliners())
+
+            let datesArr = []
+            function getDates(){
+              for(let ii = 0; ii < storedEvents.length; ii++){
+              datesArr.push(storedEvents[ii].start.local.substring(0, 10))
+              }
+              return datesArr
+            }
+            //getDates()
+            console.log("getDates()!!!" , getDates())
+
+            let venuesArr = []
+            function getVenues(){
+              for(let ii = 0; ii < storedEvents.length; ii++){
+                venuesArr.push(storedEvents[ii].name.text.substring((storedEvents[ii].name.text.lastIndexOf("*")+5), storedEvents[ii].name.text.lastIndexOf("(")-1))
+              }
+              return venuesArr
+            }
+            //getVenues()
+            console.log("getVenues()>>>>" , getVenues())
+
+            //Write a function that takes the storedEvents, and returns an Array with the sum of riders from all locations as an index an array.
+            //iterate through the storedEvents
+            //iterate through each pick up location.
+            //get the quantity and add it to the eventTotal accumulator variable for that event
+            //push the total to the totalRidersArr
+            //
+
+
+            let totalRidersArr = []
+            function getRidersTotal(){
+              for(let ii = 0; ii < storedEvents.length; ii++){
+                let ticketClasses = storedEvents[ii].ticket_classes
+                let eventTotal = 0
+                for(let ii = 0; ii < ticketClasses.length; ii++){
+                  eventTotal += ticketClasses[ii].quantity_sold
+                }
+                totalRidersArr.push(eventTotal)
+              }
+              console.log("totalRidersArr:o:o:o:o: " , totalRidersArr)
+            }
+            getRidersTotal()
+
+            //write a function that takes the storedEvents and returns an array of objects with key = pick-up location name, value = quantity_sold
+
+
+            // let ridersByLocationArr = []
+            // let ridersByLocationObj = {}
+            // function getRidersByLocation(){
+            //
+            //   for(let ii = 0; ii < storedEvents.length; ii++){
+            //     let classes = storedEvents[ii].ticket_classes
+            //     console.log("classes $$$$$$" , classes)
+            //     for (let ii = 0; ii < classes.length; ii++){
+            //      ridersByLocationObj[classes[ii].name.substring(indexof(), 15)] = classes[ii].quantity_sold
+            //     console.log("ridersByLocationObj |||||||||||" , ridersByLocationObj)
+            //     //console.log("ridersByLocation*****" , ridersByLocationObj)
+            //     //ridersByLocationArr.push(ridersByLocationObj)
+            //     }
+            //   }
+            // }
+            // getRidersByLocation()
+
+
+
+
+
+
+            //calculate fuel savings
+            //for each event get total number of riders from each location
+            //create object of roundtrip miles for each location
+            let miles = {
+              boulderRrx: 54.2,
+              focoRrx: 145.4,
+              denverRrx: 43.2,
+              boulderDen: 54.8,
+              focoDen: 126.4,
+              denverDen: 0,
+              boulder1stbank: 25.8,
+              foco1stbank: 99.6,
+              denver1stbank: 31.6,
+            }
+
+            //create object of MPG and average capacity for bus
+            let bus = {
+              mpg: 7,
+              cap: 48,
+            }
+            //create object of MPG and average capacity for car (this will be editable by user)
+            let car={
+              mpg: 30,
+              cap: 3,
+            }
+            //calculate bus fuel consumed:
+            //number of buses used = number of riders / average bus capacity
+              let busesUsed = Math.ceil(totalRidersArr[1] / bus.cap)
+            console.log("busesUsed --->>" , busesUsed)
+            //fuel per bus = roundtrip miles / bus mpg
+
+            //bus fuel consumed per event = fuel per bus * number of buses used
+            //
+            //calculate parallel universe car fuel consumed
+            //number of cars used = number of riders / average car capacity
+            //fuel per car = roundtrip miles / car MPG
+            //car fuel consumed per event = fuel per car * number of cars used
+            //
+            //calculate fuel savings for each event
+            //fuel not consumed = parallel universe fuel consumed per event - bus fuel consumed per event
+            //
+            //total fuel saved = sum of fuel not consumed throughout all events in question.
+
+            //only display date range (Last 50, 2018, Live, top 10, userInput)
+            //only display headliner === userInput
+
+
+      //Build the table!
+
+            function createMainRows(){
+              for(let ii = 0; ii < storedEvents.length; ii++){
+                let newRow = document.createElement("tr")
+                newRow.class = "btsorange"
+
+                let dateCell = document.createElement("td")
+                let dateCellContents = document.createTextNode(`${datesArr[ii]}`)
+                dateCell.appendChild(dateCellContents)
+                newRow.appendChild(dateCell)
+
+                let headlinerCell = document.createElement("td")
+                let headlinerCellContents = document.createTextNode(`${headlinersArr[ii]}`)
+                headlinerCell.appendChild(headlinerCellContents)
+                newRow.appendChild(headlinerCell)
+
+                let venueCell = document.createElement("td")
+                let venueCellContents = document.createTextNode(`${venuesArr[ii]}`)
+                venueCell.appendChild(venueCellContents)
+                newRow.appendChild(venueCell)
+
+                let ridersCell = document.createElement("td")
+                let ridersCellContents = document.createTextNode(`${totalRidersArr[ii]}`)
+                ridersCell.appendChild(ridersCellContents)
+                newRow.appendChild(ridersCell)
+
+                let savingsCell = document.createElement("td")
+                let savingsCellContents = document.createTextNode(`need`)
+                savingsCell.appendChild(savingsCellContents)
+                newRow.appendChild(savingsCell)
+
+                let tableBody = document.getElementsByTagName("tbody")[0]
+                console.log(`This is tableBody:: ${tableBody}`)
+                tableBody.appendChild(newRow)
+              }
+            }
+            createMainRows()
+
+            function sortTable() {
+              let table, rows, switching, i, x, y, shouldSwitch;
+              table = document.getElementById("myTable");
+              switching = true;
+              /*Make a loop that will continue until
+              no switching has been done:*/
+              while (switching) {
+                //start by saying: no switching is done:
+                switching = false;
+                rows = table.rows;
+                /*Loop through all table rows (except the
+                first, which contains table headers):*/
+                for (i = 1; i < (rows.length - 1); i++) {
+                  //start by saying there should be no switching:
+                  shouldSwitch = false;
+                  /*Get the two elements you want to compare,
+                  one from current row and one from the next:*/
+                  x = rows[i].getElementsByTagName("TD")[0];
+                  y = rows[i + 1].getElementsByTagName("TD")[0];
+                  //check if the two rows should switch place:
+                  if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                  }
+                }
+                if (shouldSwitch) {
+                  /*If a switch has been marked, make the switch
+                  and mark that a switch has been done:*/
+                  rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                  switching = true;
+                }
+              }
+            }
+            sortTable()
+    } else {
+      alert("press calculate to load events!")
+    }
+  }
+  loadStored()
+
+
 
 })
+
+//BONEYARD
+
+//function findHeadliner()
+
+// axios.get('https://www.eventbriteapi.com/v3/users/me/owned_events/?token=T4AWNLBLNDQD32SYGHHI&status=live&order_by=start_desc&expand=ticket_classes')
+// .then(function(response){
+//   console.log("this is response.data::" , response.data)
+//   for(let ii=0; ii < response.data.events.length; ii++){
+//     let myEvent = response.data.events[ii]
+//           console.log("this is myEvent.name.text::" , myEvent.name.text)
+//           let headliner = myEvent.name.text.substring((0), myEvent.name.text.indexOf("*")-1)
+//           console.log("this is headliner:::" , headliner)
+//           // var res = str.substring(str.indexOf("e"), str.indexOf("o"))
+//           for(let ii=0; ii < myEvent.ticket_classes.length; ii++){
+//                 let ticketClass = myEvent.ticket_classes[ii]
+//                 console.log("this is ticketClass.name::" , ticketClass.name, "this is quantity sold::" , ticketClass.quantity_sold)
+//               }
+//   }
+// })
